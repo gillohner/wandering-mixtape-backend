3e9675cb1d15a9ee932b42928977e078303a7215
Gil Lohner <gil@lohner.net>
2025-01-01 20:51:52 +0800

    reset middlewares

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,13 +1,12 @@
-module.exports = ({ env }) => ({
-  settings: {
-    cors: {
-      enabled: true,
-      origin: [
-        "http://wmt.lohner.cloud",
-        "https://wmt.lohner.cloud",
-      ],
-      headers: ["*"],
-      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    },
-  },
-});
+export default [
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
